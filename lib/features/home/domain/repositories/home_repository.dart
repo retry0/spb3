1f2ba0c94570416b8662fceaa2e7101cd9d24f9e
Lingga Adi Pratama <lingga.a.pratama@sinarmas-agri.com>
2025-07-10 16:14:29 +0700

    :fire: del unnecessarily file

--- lib/features/home/domain/repositories/home_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../../core/error/failures.dart';
-
-abstract class HomeRepository {
-  Future<Either<Failure, Map<String, dynamic>>> getDashboardMetrics();
-  Future<Either<Failure, List<Map<String, dynamic>>>> getActivities();
-}
